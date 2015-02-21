a686d61b4fa1f2b8bb8aea8d020815863d2c118e
Gabe Austin <gabriel.austin1@gmail.com>
2015-02-21 12:00:47 -0600

    working code

--- js/app.js
+++ js/app.js
@@ -2,51 +2,66 @@
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+	$(".what").click(function(){
+  	$(".overlay").fadeIn(1000);
 
-  	});
+	});
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+	/*--- Hide information modal box ---*/
+	$("a.close").click(function(){
+		$(".overlay").fadeOut(1000);
+	});
 
-    // start a new game by clicking "New Game" button
-    $(document).on("click", ".new", function(){
-      location.reload(true);
-    });
+  // start a new game by clicking "New Game" button
+  $(document).on("click", ".new", function(){
+    location.reload(true);
+  });
 
-    // creates a random # 1 - 100
-    function getRandomNumber() {
-      return Math.floor(Math.random() * 100) + 1;
-    }
+  // creates a random # 1 - 100
+  function getRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+  }
 
-    // stores the random number
-    var randomNumber = getRandomNumber();
-    $("#guessButton").click(function() {
-      var userGuess = $("#feedback").empty();
+  // stores the random number
+  var randomNumber = getRandomNumber();
+  var count = 0;
+  $("#guessButton").click(function() {
+    var userGuess = $("#userGuess").val();
 
-      // checks to see how close user is to random #
-      if (userGuess < randomNumber) {
-        // alert("Too low");
-        $("#feedback").html("Too low");
-      } else if (userGuess > randomNumber) {
-        // alert("Too high");
-        $("#feedback").html("Too high");
-      } else {
-        // alert("Just right!!!");
-        $("#feedback").html("Just right!!!");
-      }
+    //Feedback logic
+    var result = checkNumber(randomNumber, userGuess);
+    $("#feedback").html(result);
 
-    // increments count when button clicked
-    var count = 0;
-    $("#guessButton").click(function() {
-      count++;
-      $("#count").html(count);
-    });
+    //Count logic
+    count++;
+    $("#count").html(count);
+
+    //add guess number as list
+    $("ul#guessList").append("<li>" + userGuess + "</li>");
 
   });
+
+  function checkNumber (randomNumber, userGuess) {
+    // checks to see how close user is to random #
+    var diff = Math.abs(randomNumber - userGuess);
+    if ( diff > 50 ){
+      return "Ice cold";
+    } else if ( diff > 30 ) {
+      return "Cold";
+    } else if ( diff > 20) {
+      return "Warm";
+    } else if ( diff > 10) {
+      return "Hot";
+    } else if ( diff >= 1) {
+      return "Very Hot";
+    } else if (diff === 0) {
+      return "Just right!!!";
+    } else {
+      return "Keep Guessing...";
+    }
+  }
+
 });
+
 
 
